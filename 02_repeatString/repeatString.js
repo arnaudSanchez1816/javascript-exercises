c9d13c71396cb3d418d7a5c4f8a1e2d65236e667
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2024-12-16 00:40:41 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repetitionCount) {
+    if(repetitionCount === undefined || repetitionCount < 0) {
+        return "ERROR";
+    }
 
+    let result = "";
+    for (let i = 0; i < repetitionCount; i++) {
+        result += stringToRepeat;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
