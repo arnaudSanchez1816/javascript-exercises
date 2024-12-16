270950a5623b2477f7e3545b054774a4d538fb22
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2024-12-16 01:44:02 +0100

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(isNaN(year)){
+        return "ERROR";
+    }
 
+    if(year % 4 !== 0) {
+        return false;
+    }
+
+    if(year % 100 === 0 && (year % 400 === 0) === false) {
+        return false;
+    }
+
+    return true; 
 };
 
 // Do not edit below this line
