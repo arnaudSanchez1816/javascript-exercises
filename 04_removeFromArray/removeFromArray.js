1b64b75986644c4275689b343f1efd912590c5cf
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2024-12-16 01:20:00 +0100

    complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let resultArray = [];
+    for(let i = 0; i < array.length; ++i) {
+        if(toRemove.find(element => array[i] === element) === undefined) {
+            resultArray.push(array[i]);
+        }
+    }
+
+    return resultArray;
 };
 
 // Do not edit below this line
