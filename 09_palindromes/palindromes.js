bef2342a6c6bb337b294766c4e1eae6447eba0d9
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2024-12-19 03:21:08 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strFiltered = [...str.toLowerCase()]
+    .filter(x => (x >= "A" && x <= "z") || (x >= "0" && x <= "9"));
+    const reverse = strFiltered.slice().reverse().join("")
 
+    return strFiltered.join("") === reverse;
 };
 
 // Do not edit below this line
