d0f464b82d4ab4cda8bd4b66daec076f641932f2
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2024-12-19 03:32:31 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0) {
+        return "OOPS";
+    }
 
+    if(+number === 0) {
+        return 0;
+    }
+
+    if(+number === 1) {
+        return 1;
+    }
+
+    let first = 1;
+    let second = 0;
+    for(let i = 2; i <= number; ++i) {
+        let value = first + second;
+        second = first;
+        first = value;
+    }
+
+    return first;
 };
 
 // Do not edit below this line
