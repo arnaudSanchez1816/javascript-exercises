027732bb09d916867aa28d9a69bf265222461377
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2024-12-19 02:52:21 +0100

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((sum, x) => add(sum, x), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((sum, x) => sum * x, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0) {
+    return 1;
+  }
+  
+  let total = 1;
+  for(let i = number; i > 0; --i) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
