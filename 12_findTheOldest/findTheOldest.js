f815f1deee4774d2e02c8f572e52685d1ffa8754
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2024-12-20 20:51:16 +0100

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((result, person) => {
+        if(getPeopleAge(result) < getPeopleAge(person)) {
+            result = person;
+        }
+        return result;
+    });
 };
+
+function getPeopleAge(people) {
+    const birthDate = people.yearOfBirth;
+    const year = people.yearOfDeath || new Date().getFullYear();
+    return year - birthDate;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
